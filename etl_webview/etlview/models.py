d1494472a3ce26b5b028b3aca4ddeb37f460d411
Roopesh Sivam <roopeshsivam@gmail.com>
2017-01-07 00:54:01 +0400

    initial database

--- etl_webview/etlview/models.py
+++ etl_webview/etlview/models.py
@@ -3,3 +3,22 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class SerInData(models.Model):
+  hourly_date = models.DateTimeField()
+  co_cop = models.CharField(max_length=200)
+  co_tg = models.CharField(max_length=200)
+  o3_cop = models.CharField(max_length=200)
+  o3_tg = models.CharField(max_length=200)
+  no2_cop = models.CharField(max_length=200)
+  no2_tg = models.CharField(max_length=200)
+  c6h6_cop = models.CharField(max_length=200)
+  c6h6_tg = models.CharField(max_length=200)
+  temp_cop = models.CharField(max_length=200)
+  temp_tg = models.CharField(max_length=200)
+  rh_cop = models.CharField(max_length=200)
+  rh_tg = models.CharField(max_length=200)
+  noise_cop = models.CharField(max_length=200)
+  noise_tg = models.CharField(max_length=200)
+  def __str__(self):
+    return self.hourly_date
+  
